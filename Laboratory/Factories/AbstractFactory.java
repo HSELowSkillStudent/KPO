9ca18e3defca42e1804ce120eebffd4afe6ef0f4
HSELowSkillStudent <113442815+HSELowSkillStudent@users.noreply.github.com>
2023-01-28 20:56:47 +0300

    Delete Laboratory directory

--- Laboratory/Factories/AbstractFactory.java
+++
@@ -1,9 +0,0 @@
-package org.example.Factories;
-
-import org.example.Devices.Radio;
-import org.example.Devices.TV;
-
-public interface AbstractFactory {
-    TV createTV();
-    Radio createRadio();
-}
